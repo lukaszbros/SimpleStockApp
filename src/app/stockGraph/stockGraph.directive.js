210b4d7d649e89b30d5fa069d5b81c027d379223
lukaszbros <lukasz.bros@gmail.com>
2016-09-12 13:40:15 +0200

    Scale graph

--- src/app/stockGraph/stockGraph.directive.js
+++ src/app/stockGraph/stockGraph.directive.js
@@ -12,15 +12,11 @@ export function d3Graph($window, $timeout, StockGraphService) {
     },
     link: function (scope, ele, attrs) {
       StockGraphService.d3().then(function (d3) {
-        console.log(d3);
         var renderTimeout;
-        var margin = parseInt(attrs.margin) || 20,
-            barHeight = parseInt(attrs.barHeight) || 20,
-            barPadding = parseInt(attrs.barPadding) || 5;
+        var margin = parseInt(attrs.margin) || 20;
 
         var svg = d3.select(ele[0])
-            .append('svg')
-            .style('width', '100%');
+            .append('svg');
 
         $window.onresize = function () {
           scope.$apply();
@@ -42,24 +38,6 @@ export function d3Graph($window, $timeout, StockGraphService) {
           if (renderTimeout) clearTimeout(renderTimeout);
 
           renderTimeout = $timeout(function () {
-            /*
-             g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-             let parseTime = d3.timeParse('%Y-%m-%d');
-
-             let x = d3.scaleTime().range([0, width]),
-             y = d3.scaleLinear().range([height, 0]),
-             z = d3.scaleOrdinal(d3.schemeCategory10);
-
-             let line = d3.line()
-             .curve(d3.curveBasis)
-             .x(function(d) { return x(parseTime(Date.Parse(d.Date))); })
-             .y(function(d) { return y(+d.Close); });
-
-             svg.append(line(data[0].data));*/
-
-            // define dimensions of graph
-            console.log(data);
             let minValue = data[0].values[0].close,
                 maxValue = data[0].values[0].close;
             angular.forEach(data[0].values, stockData => {
